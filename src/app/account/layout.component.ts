4ea9f9145a11c2341d68df666b6851688ff812a1
Ramic Gautam <gautamramic11@gmail.com>
2021-04-10 18:30:17 +1000

    initial commit

---
+++ src/app/account/layout.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AccountService } from '@app/_services';
+
+@Component({ 
+    templateUrl: 'layout.component.html' ,
+    styles: ['.clsimg { height: 100vh }','.clsimg img { width: 100%; height: 100% }']
+ })
+export class LayoutComponent {
+    constructor(
+        private router: Router,
+        private accountService: AccountService
+    ) {
+        // redirect to home if already logged in
+        if (this.accountService.userValue) {
+            this.router.navigate(['/']);
+        }
+    }
+}
